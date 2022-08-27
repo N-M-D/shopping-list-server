e5df32784fb3612433f6cc47cabe5c2756814a5e
N-M-D <cty.chuateckyong@gmail.com>
2022-08-27 17:41:30 +0800

    Added aws-sdk to s3

--- models/s3.js
+++ models/s3.js
@@ -1,17 +1,27 @@
 require('dotenv').config();
 const fs = require('fs');
 const S3 = require("aws-sdk/clients/s3");
+const AWS = require('aws-sdk')
 
 const bucketName = process.env.AWS_BUCKET_NAME;
 const region = process.env.AWS_BUCKET_REGION
 const accessKey = process.env.AWS_ACCESS_KEY
 const secretKey = process.env.AWS_SECRET_KEY
 
-const s3 = new S3({
-    region,
-    accessKey,
-    secretKey
-});
+
+
+// aws.config.update({
+//     accessKeyId: process.env.AWSAccessKeyId,
+//     secretAccessKey: process.env.AWSSecretKey,
+//     region: process.env.AWSRegion,
+//     });
+AWS.config.update({
+    accessKeyId: accessKey,
+    secretAccessKey: secretKey,
+    region: region,
+})
+
+const s3 = new AWS.S3({})
 
 //uploads file to S3
 function uploadFile(file) {
